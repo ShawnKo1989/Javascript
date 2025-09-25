faad7217f90e73d3b1260b4e9530907d598794b3
Shawn <devshawn1989@gmail.com>
2025-09-25 13:15:03 -0700

    init add

---
+++ toyproject/exchange-currencies/main.js
@@ -0,0 +1,7 @@
+//1. making two boxes 
+//2. making dropdown lists
+//3. need currencies values for the app
+//4. when chosing the contents in dropdown box, item is changed
+//5. when input is filled, currency change is excuted.
+//6. when item is chosen, currency unit is changed with selected currency
+//7.  reading method for currencies.
